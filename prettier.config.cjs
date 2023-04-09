ecbe1c7b188f9afa0c3a05d888481309b9b0c157
michu2k <michal.strumpf@gmail.com>
2023-04-09 19:56:29 +0200

    Added custom _document.tsx, added Inter font

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,7 +2,7 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [require.resolve("prettier-plugin-tailwindcss")]
 };
 
 module.exports = config;
