bab8f2904eb3e5244672122006449c5a0469b75f
michu2k <michal.strumpf@gmail.com>
2023-04-10 19:08:26 +0200

    Use next/font instead of Google url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,33 @@
 import {type AppType} from "next/app";
 import {type Session} from "next-auth";
 import {SessionProvider} from "next-auth/react";
+import {Inter} from "next/font/google";
 
 import {api} from "~/utils/api";
 
 import "~/styles/globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"]
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: {session, ...pageProps}
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
