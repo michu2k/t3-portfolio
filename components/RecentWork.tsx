be9a49365624f4ab9f4340b2726e863a5f569056
michu2k <michal.strumpf@gmail.com>
2023-05-07 12:27:58 +0200

    RecentWork: Added link

--- components/RecentWork.tsx
+++ components/RecentWork.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import {PageSection} from "./generics/PageSection";
 
 const recentWorkItems: Array<RecentWorkItemProps> = [
@@ -43,19 +44,27 @@ type RecentWorkItemProps = {
 }
 
 const RecentWorkItem = ({id, name, description}: RecentWorkItemProps) => {
-  return (
-    <div className="w-auto rounded-lg shrink-0 flex flex-col">
-      <div className="w-auto h-56 rounded-lg bg-slate-300 shrink-0">
-        {/* <Image /> */}
-      </div>
 
-      <div className="flex flex-col items-start py-4">
-        <p className="font-semibold text-xl mb-4 inline-flex rounded-sm text-slate-700">{name}</p>
-        <p className="text-sm inline-flex rounded-sm leading-6 text-slate-600">
-          {description}
-        </p>
-      </div>
-    </div>
+  const itemPageUrl = `/portfolio/${id}`;
+
+  return (
+    <article className="w-auto rounded-lg shrink-0 flex flex-col">
+      <Link href={itemPageUrl}>
+        <div className="w-auto h-56 rounded-lg bg-slate-300 shrink-0">
+          {/* <Image /> */}
+        </div>
+
+        <div className="flex flex-col items-start py-4">
+          <p className="font-semibold text-xl mb-4 inline-flex rounded-sm text-slate-700">
+            {name}
+          </p>
+
+          <p className="text-sm inline-flex rounded-sm leading-6 text-slate-600">
+            {description}
+          </p>
+        </div>
+      </Link>
+    </article>
   );
 };
 
