34d861a120a097441f206f389d6216e6a625a7c3
michu2k <michal.strumpf@gmail.com>
2023-05-13 13:40:00 +0200

    Removed tech section, added skills to About me section

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,31 +1,59 @@
 import React from "react";
 import {PageSection} from "./generics/PageSection";
 
+const skills = [
+  "HTML",
+  "CSS",
+  "SASS",
+  "Tailwind",
+  "React",
+  "Next.js",
+  "TypeScript",
+  "JavaScript",
+  "Storybook",
+  "Framer Motion",
+  "Express",
+  "GraphQL",
+  "Prisma",
+  "Adobe XD",
+  "Figma",
+  "Cypress",
+  "Git"
+];
+
 const AboutMe = () => {
+
+  function displaySkills() {
+    return skills.map((name) => (
+      <li key={name} className="bg-neutral-50 px-4 rounded">
+        <p className="font-medium text-xs text-slate-700 leading-8">{name}</p>
+      </li>
+    ));
+  }
+
   return (
-    <PageSection upperHeading="About Me" heading="JavaScript developer & UI designer">
-      <p className="text-md text-slate-600 leading-7">
+    <PageSection heading="About Me">
+      <p className="text-md text-slate-600 leading-8">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Nihil incidunt accusamus mollitia exercitationem sapiente quasi qui eligendi architecto non dolor.
         Autem consectetur et voluptatum labore accusantium magni. Laudantium quia vitae quas provident nostrum adipisci.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, voluptates.
       </p>
 
-      <a
-        href="./cv.pdf"
-        className="
-          h-12 px-8
-          inline-flex items-center
-          rounded-md
-          mt-8
-          text-md font-semibold text-slate-900
-          border-2 border-slate-800
-          hover:text-white hover:bg-slate-800
-          transition-colors"
-        rel="noopener noreferrer"
-        target="_blank">
-        Download CV
-      </a>
+      <div className="mt-14">
+        <h3 className="font-semibold text-lg mb-3">My Skills</h3>
+        {/* <hr className="h-1 w-10 mb-4 bg-primary border-0 rounded-md" /> */}
+        <p className="text-md text-slate-600 leading-8 mb-8">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+
+        <ul className="flex flex-wrap gap-2">
+          {displaySkills()}
+          <li className="px-2">
+            <p className="text-xs text-slate-400 leading-8">& more...</p>
+          </li>
+        </ul>
+      </div>
     </PageSection>
   );
 };
