8127c19e90a8cd1a83c9e1a4270ad734f173a912
michu2k <michal.strumpf@gmail.com>
2023-05-14 19:46:47 +0200

    AboutMe: Updated text

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -44,7 +44,7 @@ const AboutMe = () => {
         <h3 className="font-semibold text-lg mb-3">My Skills</h3>
         {/* <hr className="h-1 w-10 mb-4 bg-primary border-0 rounded-md" /> */}
         <p className="text-md text-slate-600 leading-8 mb-8">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Tools and programming languages that I use daily and have experience with.
         </p>
 
         <ul className="flex flex-wrap gap-2">
