93e81966653f9baa9485f0727851e703763f092d
michu2k <michal.strumpf@gmail.com>
2023-08-05 10:09:57 +0200

    Added Button, Input & Form components

---
+++ components/Button.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import {cva, type VariantProps} from "class-variance-authority";
+
+import {cn} from "~/utils/className";
+
+const buttonVariants = cva(
+  `font-medium text-sm
+  inline-flex items-center justify-center rounded-md ring-offset-white
+  focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-300 focus-visible:ring-offset-2
+  disabled:pointer-events-none disabled:opacity-50
+  transition-colors`,
+  {
+    variants: {
+      variant: {
+        primary: "bg-slate-900 text-slate-50 hover:bg-slate-900/80",
+        secondary: "bg-slate-100 text-slate-900 hover:bg-slate-100/80",
+        destructive: "bg-red-500 text-slate-50 hover:bg-red-500/90"
+      },
+      size: {
+        sm: "h-8 px-2",
+        md: "h-10 px-4",
+        icon: "h-10 w-10"
+      }
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md"
+    }
+  }
+);
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<typeof buttonVariants>
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({className, variant, size, ...props}, ref) => {
+  return (
+    <button
+      className={cn(buttonVariants({variant, size, className}))}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
+Button.displayName = "Button";
+
+export {Button};
---
+++ components/Input.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import {cn} from "~/utils/className";
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({className, type, ...props}, ref) => {
+
+  const inputClassName = cn(
+    `flex h-10 w-full rounded-md
+    border border-input
+    bg-background px-3 py-2
+    text-sm
+    ring-offset-background
+    file:border-0 file:bg-transparent file:text-sm file:font-medium
+    placeholder:text-muted-foreground
+    focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-300 focus-visible:ring-offset-2
+    disabled:cursor-not-allowed disabled:opacity-50`,
+    className
+  );
+
+  return (
+    <input ref={ref} type={type} className={inputClassName} {...props} />
+  );
+});
+
+Input.displayName = "Input";
+
+export {Input};
