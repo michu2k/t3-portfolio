4967d8f7618f92a3a77eaf21cad6f1ab93f02eed
michu2k <michal.strumpf@gmail.com>
2023-08-15 13:17:10 +0200

    Sidebar: Added user section, logout button

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Link from "next/link";
 import {useRouter} from "next/router";
+import {signOut, useSession} from "next-auth/react";
 import type {LucideIcon} from "lucide-react";
-import {Settings, Info, Laptop2, Image, Mail, Briefcase} from "lucide-react";
+import {Settings, Info, Laptop2, Image, Mail, Briefcase, LogOut} from "lucide-react";
+import {Avatar, AvatarFallback, AvatarImage} from "~/components/ui/Avatar";
+import {Button} from "~/components/ui/Button";
+import {getUserInitials} from "~/utils/getUserInitials";
 
 const sidebarItems: Array<SidebarItemDef> = [
   {
@@ -44,7 +48,10 @@ const sidebarItems: Array<SidebarItemDef> = [
 ];
 
 const Sidebar = () => {
+  const {data: sessionData} = useSession();
   const {pathname} = useRouter();
+
+  const {name, image, email} = sessionData?.user || {};
 
   function displaySidebarItems() {
     return sidebarItems.map(({href, ...item}) => {
@@ -59,10 +66,29 @@ const Sidebar = () => {
   }
 
   return (
-    <nav className="md:sticky md:top-10">
+    <nav className="flex flex-col h-full">
+      <div className="flex items-center gap-2 mb-6 md:mx-4 min-w-0">
+        <Avatar>
+          {image && <AvatarImage src={image} alt={name || ""} />}
+          <AvatarFallback>{getUserInitials(name)}</AvatarFallback>
+        </Avatar>
+
+        <div className="min-w-0">
+          <p className="text-sm font-medium text-slate-700 overflow-hidden whitespace-nowrap text-ellipsis">{name}</p>
+          <p className="text-xs text-slate-500 overflow-hidden whitespace-nowrap text-ellipsis">{email}</p>
+        </div>
+      </div>
+
       <ul className="flex md:flex-col flex-wrap gap-x-4 gap-y-1 md:gap-y-3">
         {displaySidebarItems()}
       </ul>
+
+      <Button
+        variant="ghost"
+        className="h-11 w-full justify-start mt-8 md:mt-auto"
+        onClick={() => void signOut()}>
+        <LogOut size={16} className="mr-2" /> Log Out
+      </Button>
     </nav>
   );
 };
@@ -89,7 +115,7 @@ const SidebarItem = ({text, href, icon: Icon, isActive}: SidebarItemProps) => {
           py-3 px-4
           flex items-center gap-3
           rounded-lg
-          ${isActive ? "bg-slate-50 text-primary" : "text-slate-700"}
+          ${isActive ? "bg-slate-100 text-primary" : "text-slate-700"}
           hover:text-primary
           focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2
           transition-colors
