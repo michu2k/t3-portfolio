d1f5e1140555ec11192cee03f462660b72886634
michu2k <michal.strumpf@gmail.com>
2023-09-22 16:39:09 +0200

    Improved getSippets route, changed transformSnippetsDataIntoFormValues fn

--- utils/transformSnippetsDataIntoFormValues.ts
+++ utils/transformSnippetsDataIntoFormValues.ts
@@ -1,13 +1,9 @@
 import type {Snippet} from "@prisma/client";
 
-function transformSnippetsDataIntoFormValues(data: Array<Snippet>, formKeys: Array<string>) {
-  const filteredValues = data.filter(({name}) => formKeys.includes(name));
-
-  if (filteredValues.length) {
-    return formKeys.reduce((acc, key) => {
-      const formValue = filteredValues.find(({name}) => name === key);
-      return {...acc, [key]: formValue ? formValue.value : undefined};
-    }, {});
+// This function is used to transform the data from the database into the form values.
+function transformSnippetsDataIntoFormValues(data: Array<Snippet>) {
+  if (data.length) {
+    return data.reduce((acc, {name, value}) => ({...acc, [name]: value}), {});
   }
 
   return undefined;
