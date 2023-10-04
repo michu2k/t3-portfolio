f9a9fb7d19300f137a656dc58c79e99210560e41
michu2k <michal.strumpf@gmail.com>
2023-10-04 18:35:16 +0200

    s3: Added uploadFileToS3 fn

--- server/s3.ts
+++ server/s3.ts
@@ -1,4 +1,5 @@
 import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import {nanoid} from "nanoid";
 import {env} from "~/env.mjs";
 
 export const s3 = new S3Client({
@@ -8,3 +9,22 @@ export const s3 = new S3Client({
     secretAccessKey: env.AWS_S3_SECRET_ACCESS_KEY
   }
 });
+
+export async function uploadFileToS3(file: File, directory?: string) {
+  const key = directory ? `${directory}/${nanoid()}` : nanoid();
+
+  const command = new PutObjectCommand({
+    Bucket: env.AWS_S3_BUCKET,
+    Key: key,
+    Body: file,
+    ContentType: file.type
+  });
+
+  try {
+    await s3.send(command);
+    return {key};
+  } catch (error) {
+    console.error(error);
+    return {error};
+  }
+}
