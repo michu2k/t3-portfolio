d9ea8358b020e7b1b03022d549f01a54939bfd42
michu2k <michal.strumpf@gmail.com>
2023-10-13 16:19:59 +0200

    file: Added transformFileToFileObj helper

--- utils/file.ts
+++ utils/file.ts
@@ -1,13 +1,41 @@
 import type {Accept} from "react-dropzone";
 
-const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB
+type FileObj = {
+  name: string;
+  sizeKb: number;
+  type: string;
+  base64: string;
+};
 
-function convertBytesToMB(bytes: number) {
-  return `${parseFloat((bytes / (1024 * 1024)).toFixed(2))}MB`;
-}
+const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB
 
 const acceptedImageTypes: Accept = {
   "image/*": [".jpg", ".jpeg", ".png", ".gif"]
 };
 
-export {MAX_FILE_SIZE, acceptedImageTypes, convertBytesToMB};
+function convertBytesToMB(bytes: number) {
+  return `${parseFloat((bytes / (1024 * 1024)).toFixed(2))}MB`;
+}
+
+async function convertFileToBase64(file: File) {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
+}
+
+async function transformFileToFileObj(file: File): Promise<FileObj> {
+  return {
+    name: file.name,
+    sizeKb: Math.round(file.size / 1024),
+    type: file.type,
+    base64: await convertFileToBase64(file)
+  };
+}
+
+export type {FileObj};
+
+export {MAX_FILE_SIZE, acceptedImageTypes, transformFileToFileObj, convertBytesToMB};
