ba43b91f92b17abb75fceac8bd951376703edca3
michu2k <michal.strumpf@gmail.com>
2023-10-13 17:41:49 +0200

    server: Added functions for upload files to S3

--- server/s3.ts
+++ server/s3.ts
@@ -1,6 +1,8 @@
-import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
-import {nanoid} from "nanoid";
+import {GetObjectCommand, PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
+import {v4 as uuidv4} from "uuid";
 import {env} from "~/env.mjs";
+import {getFileExtension, type FileObj} from "~/utils/file";
 
 export const s3 = new S3Client({
   region: env.AWS_S3_REGION,
@@ -10,13 +12,17 @@ export const s3 = new S3Client({
   }
 });
 
-export async function uploadFileToS3(file: File, directory?: string) {
-  const key = directory ? `${directory}/${nanoid()}` : nanoid();
+/** Upload a new file to S3 and return its key */
+export async function uploadFileToS3(file: FileObj, directory?: string) {
+  const ext = getFileExtension(file.type);
+  const key = directory ? `${directory}/${uuidv4()}.${ext}` : `${uuidv4()}.${ext}`;
+  const base64Data = Buffer.from(file.url.replace(/^data:image\/\w+;base64,/, ""), "base64");
 
   const command = new PutObjectCommand({
     Bucket: env.AWS_S3_BUCKET,
     Key: key,
-    Body: file,
+    Body: base64Data,
+    ContentEncoding: "base64",
     ContentType: file.type
   });
 
@@ -25,6 +31,29 @@ export async function uploadFileToS3(file: File, directory?: string) {
     return {key};
   } catch (error) {
     console.error(error);
-    return {error};
+    throw new Error("An error occured while uploading files to S3");
+  }
+}
+
+/** Get presigned url for a file from S3 */
+export async function getPresignedUrl(key: string): Promise<FileObj> {
+  try {
+    const command = new GetObjectCommand({
+      Bucket: env.AWS_S3_BUCKET,
+      Key: key
+    });
+
+    const {ContentLength = 0, ContentType = "unknown"} = await s3.send(command);
+    const presignedUrl = await getSignedUrl(s3, command, {expiresIn: 3600});
+
+    return {
+      name: key,
+      url: presignedUrl,
+      sizeKb: Math.round(ContentLength / 1024),
+      type: ContentType
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error("An error occured while retrieving image from S3");
   }
 }
