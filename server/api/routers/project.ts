96071d65c47e9e6f58f34f5b81a7a45dc8886089
michu2k <michal.strumpf@gmail.com>
2023-10-15 11:56:34 +0200

    project: Added uploading & retrieving images from AWS, updated schema

--- server/api/routers/project.ts
+++ server/api/routers/project.ts
@@ -1,32 +1,76 @@
 import {z} from "zod";
+import {TRPCError} from "@trpc/server";
+import type {ProjectItem as PrismaProjectItem} from "@prisma/client";
 
 import {createTRPCRouter, protectedProcedure, publicProcedure} from "~/server/api/trpc";
+import {getPresignedUrl, uploadFileToS3} from "~/server/s3";
+import type {FileObj} from "~/utils/file";
 import {projectItemSchema} from "~/utils/validations/project";
+
+type ProjectItem = Omit<PrismaProjectItem, "coverImage" | "image"> & {
+  coverImage: FileObj;
+  image: FileObj;
+};
+
+const S3_DIRECTORY_NAME = "projects";
 
 // prettier-ignore
 export const projectRouter = createTRPCRouter({
   getItems: publicProcedure
     .query(async ({ctx}) => {
-      return await ctx.prisma.projectItem.findMany();
+      const items = await ctx.prisma.projectItem.findMany();
+
+      const itemsWithImageObjects = await Promise.all(items.map(async ({coverImage, image, ...item}) => {
+        const coverImageUrl = await getPresignedUrl(coverImage);
+        const imageUrl = await getPresignedUrl(image);
+
+        return {...item, coverImage: coverImageUrl, image: imageUrl};
+      }));
+
+      return itemsWithImageObjects;
     }),
 
   getItem: protectedProcedure
     .input(z.object({id: z.string()}))
     .query(async ({ctx, input: {id}}) => {
-      return await ctx.prisma.projectItem.findUnique({
+      const item = await ctx.prisma.projectItem.findUnique({
         where: {id}
       });
+
+      if (item) {
+        const coverImageUrl = await getPresignedUrl(item.coverImage);
+        const imageUrl = await getPresignedUrl(item.image);
+
+        return {...item, coverImage: coverImageUrl, image: imageUrl};
+      }
+
+      return null;
     }),
 
   createItem: protectedProcedure
     .input(projectItemSchema)
-    .mutation(async ({ctx, input}) => {
-      return await ctx.prisma.projectItem.create({
-        data: input
-      });
-    }),
+    .mutation(async ({ctx, input: {image, coverImage, ...input}}) => {
+      if (!image || !coverImage ) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Image and coverImage fields are required"
+        });
+      }
 
-  updateItem: protectedProcedure
+      const {key: coverImageKey} = await uploadFileToS3(coverImage, S3_DIRECTORY_NAME);
+      const {key: imageKey} = await uploadFileToS3(image, S3_DIRECTORY_NAME);
+
+      return await ctx.prisma.projectItem.create({
+        data: {
+          ...input,
+          coverImage: coverImageKey,
+          image: imageKey
+        }
+      });
+    })
+
+  // WIP
+/*   updateItem: protectedProcedure
     .input(projectItemSchema)
     .mutation(async ({ctx, input: {id, ...input}}) => {
       return await ctx.prisma.projectItem.update({
@@ -35,11 +79,14 @@ export const projectRouter = createTRPCRouter({
       });
     }),
 
+  // WIP
   deleteItem: protectedProcedure
     .input(z.object({id: z.string()}))
     .mutation(async ({ctx, input: {id}}) => {
       return await ctx.prisma.projectItem.delete({
         where: {id}
       });
-    })
+    }) */
 });
+
+export type {ProjectItem};
--- utils/validations/project.ts
+++ utils/validations/project.ts
@@ -7,9 +7,15 @@ const projectItemSchema = z.object({
   id: z.string().optional(),
   name: z.string().min(3, "Name must be at least 3 characters long"),
   description: z.string().min(3, "Description must be at least 3 characters long"),
-  shortDescription: z.string().optional(),
-  image: z.custom<FileObj>().refine((file) => !!file, {message: "Image is required"}),
-  coverImage: z.custom<FileObj>().refine((file) => !!file, {message: "Cover image is required"})
+  shortDescription: z.string().nullable().optional(),
+  image: z
+    .custom<FileObj>()
+    .optional()
+    .refine((file) => !!file, {message: "Image is required"}),
+  coverImage: z
+    .custom<FileObj>()
+    .optional()
+    .refine((file) => !!file, {message: "Cover image is required"})
 });
 
 export type {ProjectItemFormValues};
