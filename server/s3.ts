f097f2e3e5d020b5ae31cb1e36c1d760390e6429
michu2k <michal.strumpf@gmail.com>
2023-10-22 11:22:07 +0200

    s3: Added deleteFileFromS3 function

--- server/s3.ts
+++ server/s3.ts
@@ -1,4 +1,4 @@
-import {GetObjectCommand, PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import {DeleteObjectCommand, GetObjectCommand, PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
 import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
 import {v4 as uuidv4} from "uuid";
 import {env} from "~/env.mjs";
@@ -14,24 +14,24 @@ export const s3 = new S3Client({
 
 /** Upload a new file to S3 and return its key */
 export async function uploadFileToS3(file: FileObj, directory?: string) {
-  const ext = getFileExtension(file.type);
-  const key = directory ? `${directory}/${uuidv4()}.${ext}` : `${uuidv4()}.${ext}`;
-  const base64Data = Buffer.from(file.url.replace(/^data:image\/\w+;base64,/, ""), "base64");
-
-  const command = new PutObjectCommand({
-    Bucket: env.AWS_S3_BUCKET,
-    Key: key,
-    Body: base64Data,
-    ContentEncoding: "base64",
-    ContentType: file.type
-  });
-
   try {
+    const ext = getFileExtension(file.type);
+    const key = directory ? `${directory}/${uuidv4()}.${ext}` : `${uuidv4()}.${ext}`;
+    const base64Data = Buffer.from(file.url.replace(/^data:image\/\w+;base64,/, ""), "base64");
+
+    const command = new PutObjectCommand({
+      Bucket: env.AWS_S3_BUCKET,
+      Key: key,
+      Body: base64Data,
+      ContentEncoding: "base64",
+      ContentType: file.type
+    });
+
     await s3.send(command);
     return {key};
   } catch (error) {
     console.error(error);
-    throw new Error("An error occured while uploading files to S3");
+    throw new Error("An error occured while uploading file to S3");
   }
 }
 
@@ -44,7 +44,7 @@ export async function getPresignedUrl(key: string): Promise<FileObj> {
     });
 
     const {ContentLength = 0, ContentType = "unknown"} = await s3.send(command);
-    const presignedUrl = await getSignedUrl(s3, command, {expiresIn: 3600});
+    const presignedUrl = await getSignedUrl(s3, command, {expiresIn: 3600}); // 1 hour
 
     return {
       name: key,
@@ -54,6 +54,21 @@ export async function getPresignedUrl(key: string): Promise<FileObj> {
     };
   } catch (error) {
     console.error(error);
-    throw new Error("An error occured while retrieving image from S3");
+    throw new Error(`An error occured while retrieving "${key}" from S3`);
+  }
+}
+
+/** Delete a file from S3 */
+export async function deleteFileFromS3(key: string) {
+  try {
+    const command = new DeleteObjectCommand({
+      Bucket: env.AWS_S3_BUCKET,
+      Key: key
+    });
+
+    await s3.send(command);
+  } catch (error) {
+    console.error(error);
+    throw new Error(`An error occured while deleting "${key}" from S3`);
   }
 }
