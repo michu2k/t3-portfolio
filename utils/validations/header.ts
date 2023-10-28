9ad582985d2da6f4301adda715265445329f02a0
michu2k <michal.strumpf@gmail.com>
2023-10-28 14:15:17 +0200

    Updated header schema

--- utils/validations/header.ts
+++ utils/validations/header.ts
@@ -1,10 +1,15 @@
 import {z} from "zod";
+import type {FileObj} from "../file";
 
 type HeaderSnippetsFormValues = z.infer<typeof headerSnippetsSchema>;
 
 const headerSnippetsSchema = z.object({
-  heading: z.string().optional(),
-  description: z.string().optional()
+  heading: z.string().min(3, "Heading must be at least 3 characters long"),
+  description: z.string().optional(),
+  image: z
+    .custom<FileObj>()
+    .optional()
+    .refine((file) => !!file, {message: "Image is required"})
 });
 
 export type {HeaderSnippetsFormValues};
