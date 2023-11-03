16866c219ba6702107b4e6f081bbfcdbfd337992
michu2k <michal.strumpf@gmail.com>
2023-11-03 11:26:36 +0100

    Header: Working image mutation, submit fn refactoring

--- utils/validations/header.ts
+++ utils/validations/header.ts
@@ -6,10 +6,7 @@ type HeaderSnippetsFormValues = z.infer<typeof headerSnippetsSchema>;
 const headerSnippetsSchema = z.object({
   heading: z.string().min(3, "Heading must be at least 3 characters long"),
   description: z.string().optional(),
-  image: z
-    .custom<FileObj>()
-    .optional()
-    .refine((file) => !!file, {message: "Image is required"})
+  image: z.custom<FileObj>().optional()
 });
 
 export type {HeaderSnippetsFormValues};
