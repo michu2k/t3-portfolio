2fe58c526d73ba8677cdcc29f342f025e71a51ae
michu2k <michal.strumpf@gmail.com>
2023-11-12 14:03:22 +0100

    Updated form layout headings

--- pages/dashboard/contact/[id].tsx
+++ pages/dashboard/contact/[id].tsx
@@ -5,15 +5,23 @@ import Head from "next/head";
 import {Layout} from "~/components/dashboard/Layout";
 import {ContactItemForm} from "~/components/dashboard/ContactItemForm";
 import {authRedirectToSignInPage} from "~/utils/authRedirect";
+import {useRouter} from "next/router";
 
 const Page: NextPage = () => {
+  const {query} = useRouter();
+  const itemId = query.id as string;
+
+  const isNew = itemId === "new";
+  const heading = isNew ? "Create" : "Edit";
+  const description = isNew ? "Create a new contact method." : "Edit an existing contact method.";
+
   return (
     <>
       <Head>
         <title>T3 Portfolio: Contact</title>
       </Head>
 
-      <Layout heading="Edit item" description="Create a new record or edit an existing one.">
+      <Layout heading={heading} description={description}>
         <ContactItemForm />
       </Layout>
     </>
--- pages/dashboard/social-media/[id].tsx
+++ pages/dashboard/social-media/[id].tsx
@@ -1,19 +1,27 @@
 import type {GetServerSidePropsContext} from "next";
 import {type NextPage} from "next";
 import Head from "next/head";
+import {useRouter} from "next/router";
 
 import {Layout} from "~/components/dashboard/Layout";
 import {SocialMediaItemForm} from "~/components/dashboard/SocialMediaItemForm";
 import {authRedirectToSignInPage} from "~/utils/authRedirect";
 
 const Page: NextPage = () => {
+  const {query} = useRouter();
+  const itemId = query.id as string;
+
+  const isNew = itemId === "new";
+  const heading = isNew ? "Create" : "Edit";
+  const description = isNew ? "Create a new social media link." : "Edit an existing link.";
+
   return (
     <>
       <Head>
         <title>T3 Portfolio: Social Media</title>
       </Head>
 
-      <Layout heading="Edit item" description="Create a new record or edit an existing one.">
+      <Layout heading={heading} description={description}>
         <SocialMediaItemForm />
       </Layout>
     </>
