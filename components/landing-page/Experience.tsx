670e38b8f2a4156323e81e5e9e5c2aa92257b504
michu2k <michal.strumpf@gmail.com>
2023-11-17 15:55:36 +0100

    landing-page: Added data to the experience section

--- components/landing-page/Experience.tsx
+++ components/landing-page/Experience.tsx
@@ -1,47 +1,16 @@
 import React from "react";
 import {PageSection} from "~/components/ui/PageSection";
 import {format} from "date-fns";
-
-const experienceItems: Array<ExperienceItemProps> = [
-  {
-    id: 1,
-    from: new Date("2019, 8, 1").toString(),
-    to: null,
-    position: "Lead Javascript Developer",
-    companyName: "Lorem Software",
-    responsibilities: [
-      "Managing a team of 5 developers, including code reviews and mentoring",
-      "Implementing new features to the XYZ application",
-      "Creating a new application for the company's internal use"
-    ]
-  },
-  {
-    id: 2,
-    from: new Date("2016, 3, 1").toString(),
-    to: new Date("2019, 8, 1").toString(),
-    position: "Javascript Developer",
-    companyName: "Lorem Software",
-    responsibilities: [
-      "Implemented interactive features and animations to enhance user experience.",
-      "Developed and maintained the company's website",
-      "Preparing & maintaining documentation for the XYZ application"
-    ]
-  },
-  {
-    id: 3,
-    from: new Date("2015, 11, 1").toString(),
-    to: new Date("2016, 3, 1").toString(),
-    position: "Junior Frontend Developer",
-    companyName: "IpsumCode",
-    responsibilities: [
-      "Translated design mockups into pixel-perfect, responsive web interfaces using HTML, CSS, and JavaScript"
-    ]
-  }
-];
+import {api} from "~/utils/api";
+import type {ExperienceItem, ExperienceItemResponsibility} from "@prisma/client";
 
 const Experience = () => {
+  const {data: experienceItems = []} = api.experience.getItems.useQuery({
+    include: {responsibilities: true}
+  });
+
   function displayExperience() {
-    return experienceItems.map((item) => <ExperienceItem key={item.id} {...item} />);
+    return experienceItems.map((item) => <ExperienceListItem key={item.id} {...item} />);
   }
 
   return (
@@ -51,42 +20,39 @@ const Experience = () => {
   );
 };
 
-type ExperienceItemProps = {
-  id: number;
-  from: string | null;
-  to: string | null;
-  companyName: string;
-  position: string;
-  responsibilities?: Array<string>;
+type ExperienceListItemProps = ExperienceItem & {
+  responsibilities?: Array<ExperienceItemResponsibility>;
 };
 
-const ExperienceItem = ({from, to, companyName, position, responsibilities}: ExperienceItemProps) => {
+const ExperienceListItem = ({startDate, endDate, company, position, responsibilities}: ExperienceListItemProps) => {
   return (
     <li className="flex flex-col items-start gap-4 md:flex-row md:gap-6">
       <div className="flex flex-col justify-center md:min-h-[4rem]">
         <span className="flex-inline min-w-[10rem] rounded-full bg-neutral-200 px-4 py-2 text-center text-xs text-slate-700">
-          {from ? format(new Date(from), "MMM yyyy") : "-"} {" - "}
-          {to ? format(new Date(to), "MMM yyyy") : <strong>Now</strong>}
+          {format(new Date(startDate), "MMM yyyy")} {" - "}
+          {endDate ? format(new Date(endDate), "MMM yyyy") : <strong>Now</strong>}
         </span>
       </div>
 
       <div className="flex flex-col gap-4">
         <div className="flex flex-col justify-center md:min-h-[4rem]">
           <p className="text-lg font-semibold leading-8 text-slate-700">{position}</p>
-          <p className="text-sm font-semibold leading-7 text-slate-400">{companyName}</p>
+          <p className="text-sm font-semibold leading-7 text-slate-400">{company}</p>
         </div>
 
-        <ul>
-          {responsibilities?.map((item, idx) => (
-            <li key={idx} className="flex items-start gap-2">
-              <span className="before:mt-3.5 before:block before:h-1 before:w-1 before:rounded-full before:bg-slate-500" />
-              <p className="text-sm leading-8">{item}</p>
-            </li>
-          ))}
-        </ul>
+        {responsibilities?.length ? (
+          <ul>
+            {responsibilities.map(({id, name}) => (
+              <li key={id} className="flex items-start gap-2">
+                <span className="before:mt-3.5 before:block before:h-1 before:w-1 before:rounded-full before:bg-slate-500" />
+                <p className="text-sm leading-8">{name}</p>
+              </li>
+            ))}
+          </ul>
+        ) : null}
       </div>
     </li>
   );
 };
 
-export {Experience, experienceItems};
+export {Experience};
