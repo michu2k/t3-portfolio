e10109d360a0f348aff931f3b6e211d973fdd5ab
michu2k <michal.strumpf@gmail.com>
2023-11-18 10:08:31 +0100

    validations: More restricted string validations

--- utils/validations/experience.ts
+++ utils/validations/experience.ts
@@ -5,14 +5,14 @@ type ExperienceItemFormValues = z.infer<typeof experienceItemSchema>;
 
 const experienceItemResponsibilitySchema = z.object({
   id: z.string().optional(),
-  name: z.string()
+  name: z.string().max(64, "Name is too long")
 });
 
 const experienceItemSchema = z
   .object({
     id: z.string().optional(),
-    position: z.string().min(3, "Position name must be at least 3 characters long"),
-    company: z.string().min(3, "Company name must be at least 3 characters long"),
+    position: z.string().min(3, "Position name must be at least 3 characters long").max(64, "Name is too long"),
+    company: z.string().min(3, "Company name must be at least 3 characters long").max(64, "Name is too long"),
     startDate: z.date({required_error: "Start date is required"}),
     endDate: z.date().nullable().optional(),
     responsibilities: z.array(experienceItemResponsibilitySchema)
--- utils/validations/header.ts
+++ utils/validations/header.ts
@@ -4,8 +4,8 @@ import type {FileObj} from "../file";
 type HeaderSnippetsFormValues = z.infer<typeof headerSnippetsSchema>;
 
 const headerSnippetsSchema = z.object({
-  heading: z.string().min(3, "Heading must be at least 3 characters long"),
-  description: z.string().optional(),
+  heading: z.string().min(3, "Heading must be at least 3 characters long").max(64, "Heading is too long"),
+  description: z.string().max(480, "Description is too long").optional(),
   image: z.custom<FileObj>().optional()
 });
 
