4b58a9047db55723230de6abe11a2132e1dc088f
michu2k <michal.strumpf@gmail.com>
2023-11-18 10:36:02 +0100

    landing-page: Added data to the recent work section

--- components/landing-page/RecentWork.tsx
+++ components/landing-page/RecentWork.tsx
@@ -1,76 +1,16 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import type {ProjectItem} from "~/server/api/routers/project";
 import {PageSection} from "~/components/ui/PageSection";
 import ArrowUpRightSvg from "~/public/svgs/arrow-up-right.svg";
-
-const recentWorkItems: Array<RecentWorkItemProps> = [
-  {
-    id: 1,
-    name: "Lorem",
-    description:
-      "Consistency across products with a design system. Improved developer experience and more efficient engineering.",
-    tags: ["React", "Tailwind", "Typescript"],
-    image: {
-      thumbnail: "https://picsum.photos/id/14/600/400",
-      full: "https://picsum.photos/id/14/2500/1667"
-    }
-  },
-  {
-    id: 2,
-    name: "Lorem Ipsum",
-    description: "Refresh the website and administration panel using the Tailwind framework.",
-    tags: ["Next.js", "Tailwind", "Typescript"],
-    image: {
-      thumbnail: "https://picsum.photos/id/27/600/400",
-      full: "https://picsum.photos/id/27//3264/1830"
-    }
-  },
-  {
-    id: 3,
-    name: "Dolor Sit Amet",
-    description: "Consistency across products with a design system.",
-    tags: ["Next.js", "Styled components", "Typescript"],
-    image: {
-      thumbnail: "https://picsum.photos/id/58/600/400",
-      full: "https://picsum.photos/id/58/1280/853"
-    }
-  },
-  {
-    id: 4,
-    name: "Lorem Ipsum",
-    description: "Refresh the website and administration panel using the Tailwind framework.",
-    tags: ["Wordpress", "Javascript", "Bulma", "SCSS"],
-    image: {
-      thumbnail: "https://picsum.photos/id/68/600/400",
-      full: "https://picsum.photos/id/68/4608/3072"
-    }
-  },
-  {
-    id: 5,
-    name: "Dolor Sit Amet",
-    description: "Consistency across products with a design system.",
-    tags: ["React", "Tailwind", "Typescript"],
-    image: {
-      thumbnail: "https://picsum.photos/id/250/600/400",
-      full: "https://picsum.photos/id/250/4928/3264"
-    }
-  },
-  {
-    id: 6,
-    name: "Dolor Sit Amet & Hello World",
-    description: "Consistency across products with a design system.",
-    tags: ["React", "Typescript"],
-    image: {
-      thumbnail: "https://picsum.photos/id/244/600/400",
-      full: "https://picsum.photos/id/244/4288/2848"
-    }
-  }
-];
+import {api} from "~/utils/api";
 
 const RecentWork = () => {
+  const {data: projectItems = []} = api.project.getItems.useQuery();
+
   function displayRecentWorkItems() {
-    return recentWorkItems.map((item) => <RecentWorkItem key={item.id} {...item} />);
+    return projectItems.map((item) => <ProjectCard key={item.id} {...item} />);
   }
 
   return (
@@ -80,26 +20,13 @@ const RecentWork = () => {
   );
 };
 
-type RecentWorkItemImage = {
-  thumbnail: string;
-  full: string;
-};
-
-type RecentWorkItemProps = {
-  id: number;
-  name: string;
-  description: string;
-  tags: Array<string>;
-  image: RecentWorkItemImage;
-};
-
-const RecentWorkItem = ({id, name, description, image}: RecentWorkItemProps) => {
+const ProjectCard = ({id, name, shortDescription, description, coverImage}: ProjectItem) => {
   const itemPageUrl = `/projects/${id}`;
 
   return (
     <article className="group w-auto shrink-0 break-inside-avoid-column">
       <div className="relative h-64 w-auto shrink-0 rounded bg-slate-100">
-        <Image src={image.thumbnail} fill className="rounded" style={{objectFit: "cover"}} alt="" />
+        <Image src={coverImage.url} fill className="rounded" style={{objectFit: "cover"}} alt="" />
       </div>
 
       <div className="flex flex-col items-start py-4">
@@ -111,10 +38,12 @@ const RecentWorkItem = ({id, name, description, image}: RecentWorkItemProps) => 
           </span>
         </Link>
 
-        <p className="text-sm leading-6">{description}</p>
+        <p className="line-clamp-3 overflow-hidden text-ellipsis text-sm leading-6">
+          {shortDescription || description}
+        </p>
       </div>
     </article>
   );
 };
 
-export {RecentWork, recentWorkItems};
+export {RecentWork};
