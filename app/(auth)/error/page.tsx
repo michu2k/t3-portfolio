1444407fee89f6ef597b4797a94346c8ec6d108a
michu2k <michal.strumpf@gmail.com>
2024-01-06 13:15:05 +0100

    Added allowed email addresses to the .env file, signIn callback, updated auth layout

---
+++ app/(auth)/error/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import {useSearchParams} from "next/navigation";
+import {buttonVariants} from "~/components/ui/button";
+import {Heading} from "~/components/ui/heading";
+import {cn} from "~/utils/className";
+
+// NextAuth errors
+const errors = {
+  AccessDenied: "You do not have permission to sign in.",
+  Verification: "Unable to verify your email address.",
+  default: "Unable to sign in."
+} as const;
+
+export default function Page() {
+  const searchParams = useSearchParams();
+
+  const error = searchParams.get("error");
+
+  return (
+    <>
+      <header className="flex w-full flex-col items-center gap-4">
+        <Heading as="h1" size="xl">
+          {error ?? "Error"}
+        </Heading>
+        <p className="text-center text-sm">{error && (errors[error as keyof typeof errors] ?? errors.default)}</p>
+      </header>
+
+      <div className="flex w-full flex-col justify-center gap-4 pb-12 pt-8">
+        <Link href="/sign-in" className={cn(buttonVariants({variant: "secondary"}))}>
+          Go back to Sign In page
+        </Link>
+      </div>
+    </>
+  );
+}
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import type {Metadata} from "next";
 import {redirect} from "next/navigation";
 import {getServerAuthSession} from "~/server/auth";
+import pkg from "~/package.json";
 
 export const metadata: Metadata = {
-  title: "Dashboard: Sign in",
+  title: "Dashboard: Authentication",
   description: "Simple, user-friendly interface for portfolio management"
 };
 
@@ -15,5 +16,13 @@ export default async function SignInLayout({children}: {children: React.ReactNod
     redirect("/dashboard");
   }
 
-  return <main className="mx-auto flex min-h-full items-center justify-center px-4">{children}</main>;
+  return (
+    <div className="mx-auto flex min-h-full max-w-80 flex-col items-center justify-center px-4">
+      {children}
+
+      <footer className="flex flex-col items-center">
+        <span className="text-xs text-slate-400">Dashboard v{pkg.version}</span>
+      </footer>
+    </div>
+  );
 }
