12e01f9714b95b9df11695922069c43fe5d66b1c
michu2k <michal.strumpf@gmail.com>
2024-03-24 15:10:39 +0100

    Header section snippets query called from the server component

--- app/(dashboard)/dashboard/header/page.tsx
+++ app/(dashboard)/dashboard/header/page.tsx
@@ -3,17 +3,20 @@ import type {Metadata} from "next";
 import {PageHeader} from "~/components/dashboard/layouts/page-header";
 import {PageContent} from "~/components/dashboard/layouts/page-content";
 import {HeaderForm} from "~/components/dashboard/forms/header-form";
+import {api} from "~/trpc/server";
 
 export const metadata: Metadata = {
   title: "Dashboard: Header"
 };
 
-export default function Page() {
+export default async function Page() {
+  const snippetsData = await api.snippet.getSnippets.query({type: "HEADER", keys: ["heading", "description", "image"]});
+
   return (
     <>
       <PageHeader heading="Header" description="Header section settings" />
       <PageContent>
-        <HeaderForm />
+        <HeaderForm snippets={snippetsData} />
       </PageContent>
     </>
   );
