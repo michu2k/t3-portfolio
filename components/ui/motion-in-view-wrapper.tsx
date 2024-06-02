0c0381d8690748297683ca9041dda99872741e03
michu2k <michal.strumpf@gmail.com>
2024-06-02 19:07:23 +0200

    MotionInViewWrapper: Lazy load framer motion features

--- components/ui/motion-in-view-wrapper.tsx
+++ components/ui/motion-in-view-wrapper.tsx
@@ -3,7 +3,7 @@
 import type {PropsWithChildren} from "react";
 import React from "react";
 import type {HTMLMotionProps} from "framer-motion";
-import {motion} from "framer-motion";
+import {domAnimation, LazyMotion, m} from "framer-motion";
 
 import {inViewAnimationProps} from "~/utils/animations";
 
@@ -11,15 +11,17 @@ type MotionInViewWrapperProps = PropsWithChildren<HTMLMotionProps<"div">>;
 
 const MotionInViewWrapper = ({children, transition, ...props}: MotionInViewWrapperProps) => {
   return (
-    <motion.div
-      {...inViewAnimationProps}
-      transition={{
-        ...inViewAnimationProps.transition,
-        ...transition
-      }}
-      {...props}>
-      {children}
-    </motion.div>
+    <LazyMotion features={domAnimation}>
+      <m.div
+        {...inViewAnimationProps}
+        transition={{
+          ...inViewAnimationProps.transition,
+          ...transition
+        }}
+        {...props}>
+        {children}
+      </m.div>
+    </LazyMotion>
   );
 };
 
