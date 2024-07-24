2d94f6b3cfafddb5fb134cb19e7a7a7b562a43f1
michu2k <michal.strumpf@gmail.com>
2024-07-24 17:35:12 +0200

    ImageCard: Display a folder icon if the given file is not an image

--- components/ui/image-card.tsx
+++ components/ui/image-card.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {FileIcon} from "lucide-react";
 import Image from "next/image";
 
 import {cn} from "~/utils/className";
@@ -12,13 +13,17 @@ type ImageCardProps = {
 };
 
 const ImageCard = React.forwardRef<HTMLDivElement, ImageCardProps>(({file, className, actions}, ref) => {
+  const isImage = file.type.includes("image");
+
   return (
     <div ref={ref} className={cn("flex min-h-[5.5rem] gap-4", className)}>
-      <div className="relative h-24 w-36 shrink-0 overflow-hidden rounded-md bg-accent">
-        {file.url ? (
+      <div className="relative flex h-24 w-36 shrink-0 items-center justify-center overflow-hidden rounded-md bg-accent">
+        {file.url && isImage ? (
           // Priority is set to true by default as the component will always be above the fold
           <Image src={file.url} fill style={{objectFit: "cover"}} sizes="192px" alt="" priority />
-        ) : null}
+        ) : (
+          <FileIcon className="size-12 stroke-1 text-muted-foreground" />
+        )}
       </div>
 
       <div className="flex min-w-0 flex-1 flex-col items-start">
