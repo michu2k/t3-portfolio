86a67b60e82522fea3ac835975781c798905d016
michu2k <michal.strumpf@gmail.com>
2024-08-25 16:27:03 +0200

    social-media: Added skeletons, adjusted list styles

---
+++ app/(dashboard)/dashboard/social-media/loading.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import {PageContent} from "~/components/dashboard/layouts/page-content";
+import {PageHeader} from "~/components/dashboard/layouts/page-header";
+import {SocialMediaListSkeleton} from "~/components/dashboard/lists/social-media-list";
+
+export default async function Page() {
+  return (
+    <>
+      <PageHeader heading="Social media" description="Social media settings" />
+      <PageContent>
+        <SocialMediaListSkeleton />
+      </PageContent>
+    </>
+  );
+}
