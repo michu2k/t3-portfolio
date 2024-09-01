7e4ea0f94028f61b1a452e91dd3b4ff79e35b5bc
michu2k <michal.strumpf@gmail.com>
2024-09-01 13:39:25 +0200

    experience: Added skeletons to the item page, replaced loading.tsx with Suspense

--- app/(dashboard)/dashboard/experience/page.tsx
+++ app/(dashboard)/dashboard/experience/page.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, {Suspense} from "react";
 import type {Metadata} from "next";
 
 import {PageContent} from "~/components/dashboard/layouts/page-content";
 import {PageHeader} from "~/components/dashboard/layouts/page-header";
-import {ExperienceList} from "~/components/dashboard/lists/experience-list";
+import {ExperienceList, ExperienceListSkeleton} from "~/components/dashboard/lists/experience-list";
 import {ensureAuthenticated} from "~/server/auth";
 import {api} from "~/trpc/server";
 
@@ -14,14 +14,20 @@ export const metadata: Metadata = {
 export default async function Page() {
   await ensureAuthenticated();
 
-  const experience = await api.experience.getItems();
-
   return (
     <>
       <PageHeader heading="Experience" description="Experience section settings" />
       <PageContent>
-        <ExperienceList experience={experience} />
+        <Suspense fallback={<ExperienceListSkeleton />}>
+          <ExperienceListWrapper />
+        </Suspense>
       </PageContent>
     </>
   );
 }
+
+const ExperienceListWrapper = async () => {
+  const experience = await api.experience.getItems();
+
+  return <ExperienceList experience={experience} />;
+};
