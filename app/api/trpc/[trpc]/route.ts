6e91f96cbcce3d218cd75f29b6996dc3543db299
michu2k <michal.strumpf@gmail.com>
2025-06-21 14:34:37 +0200

    Updated trpc logic

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,9 +1,9 @@
-import {fetchRequestHandler} from "@trpc/server/adapters/fetch";
-import {type NextRequest} from "next/server";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { type NextRequest } from "next/server";
 
-import {env} from "~/env";
-import {appRouter} from "~/server/api/root";
-import {createTRPCContext} from "~/server/api/trpc";
+import { env } from "~/env";
+import { appRouter } from "~/server/api/root";
+import { createTRPCContext } from "~/server/api/trpc";
 
 const createContext = async (req: NextRequest) => {
   return createTRPCContext({
@@ -19,8 +19,8 @@ const handler = (req: NextRequest) =>
     createContext: () => createContext(req),
     onError:
       env.NODE_ENV === "development"
-        ? ({path, error}) => console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`)
+        ? ({ path, error }) => console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`)
         : undefined
   });
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
