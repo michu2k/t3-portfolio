00517ead3e12f3389b087c2a873ca4c595d86ce1
michu2k <michal.strumpf@gmail.com>
2025-06-21 15:04:57 +0200

    auth: Revert auth route changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,7 @@
-import { handlers } from "~/server/auth";
+import NextAuth from "next-auth";
 
-export const { GET, POST } = handlers;
+import { authOptions } from "~/server/auth";
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
