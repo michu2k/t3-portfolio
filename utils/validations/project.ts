05740c6daab26219a0347ef024f44247fd55d3f5
michu2k <michal.strumpf@gmail.com>
2025-06-28 11:58:12 +0200

    Updated project schema

--- utils/validations/project.ts
+++ utils/validations/project.ts
@@ -10,14 +10,8 @@ const projectItemSchema = z.object({
   shortDescription: z.string().max(640, "Description is too long").optional(),
   description: z.string().min(3, "Description must be at least 3 characters long"),
   websiteUrl: z.string().url("URL is not valid").or(z.literal("")).optional(),
-  image: z
-    .custom<FileObj>()
-    .optional()
-    .refine((file) => !!file, { message: "Image is required" }),
-  coverImage: z
-    .custom<FileObj>()
-    .optional()
-    .refine((file) => !!file, { message: "Cover image is required" })
+  image: z.custom<FileObj>((file) => !!file, { message: "Image is required" }),
+  coverImage: z.custom<FileObj>((file) => !!file, { message: "Cover image is required" })
 });
 
 export type { ProjectItemFormValues };
