6d3977aae17c3c5cd7ad36473227e3734e5d0f84
michu2k <michal.strumpf@gmail.com>
2025-10-24 17:45:40 +0200

    Increased textarea min height

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -6,7 +6,7 @@ const Textarea = ({ className, ...props }: React.ComponentProps<"textarea">) => 
   return (
     <textarea
       className={cn(
-        "border-muted text-muted-foreground placeholder:text-placeholder focus-visible:ring-appearance flex min-h-28 w-full rounded-md border bg-transparent px-3 py-2 text-sm leading-6 focus-visible:ring-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        "border-muted text-muted-foreground placeholder:text-placeholder focus-visible:ring-appearance flex min-h-32 w-full rounded-md border bg-transparent px-3 py-2 text-sm leading-6 focus-visible:ring-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
