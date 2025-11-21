b6a78da8d886ebab1615a47eff924c75e12bb4b3
michu2k <michal.strumpf@gmail.com>
2025-11-21 16:00:48 +0100

    Added breadcrumb component

--- components/ui/breadcrumb.tsx
+++ components/ui/breadcrumb.tsx
@@ -4,69 +4,55 @@ import { ChevronRight, MoreHorizontal } from "lucide-react";
 
 import { cn } from "~/utils/cn";
 
-const Breadcrumb = React.forwardRef<
-  HTMLElement,
-  React.ComponentPropsWithoutRef<"nav"> & {
-    separator?: React.ReactNode;
-  }
->(({ ...props }, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />);
-Breadcrumb.displayName = "Breadcrumb";
+export const Breadcrumb = ({
+  ...props
+}: React.ComponentProps<"nav"> & {
+  separator?: React.ReactNode;
+}) => <nav aria-label="breadcrumb" {...props} />;
 
-const BreadcrumbList = React.forwardRef<HTMLOListElement, React.ComponentPropsWithoutRef<"ol">>(
-  ({ className, ...props }, ref) => (
-    <ol
-      ref={ref}
-      className={cn(
-        "text-muted-foreground flex flex-wrap items-center gap-1.5 text-sm break-words sm:gap-2.5",
-        className
-      )}
-      {...props}
-    />
-  )
+export const BreadcrumbList = ({ className, ...props }: React.ComponentProps<"ol">) => (
+  <ol
+    className={cn(
+      "text-muted-foreground flex flex-wrap items-center gap-1.5 text-sm break-words sm:gap-2.5",
+      className
+    )}
+    {...props}
+  />
 );
-BreadcrumbList.displayName = "BreadcrumbList";
 
-const BreadcrumbItem = React.forwardRef<HTMLLIElement, React.ComponentPropsWithoutRef<"li">>(
-  ({ className, ...props }, ref) => (
-    <li ref={ref} className={cn("inline-flex items-center gap-1.5", className)} {...props} />
-  )
+export const BreadcrumbItem = ({ className, ...props }: React.ComponentProps<"li">) => (
+  <li className={cn("inline-flex items-center gap-1.5", className)} {...props} />
 );
-BreadcrumbItem.displayName = "BreadcrumbItem";
 
-const BreadcrumbLink = React.forwardRef<
-  HTMLAnchorElement,
-  React.ComponentPropsWithoutRef<"a"> & {
-    asChild?: boolean;
-  }
->(({ asChild, className, ...props }, ref) => {
+export const BreadcrumbLink = ({
+  asChild,
+  className,
+  ...props
+}: React.ComponentProps<"a"> & {
+  asChild?: boolean;
+}) => {
   const Comp = asChild ? Slot : "a";
 
-  return <Comp ref={ref} className={cn("hover:text-foreground transition-colors", className)} {...props} />;
-});
-BreadcrumbLink.displayName = "BreadcrumbLink";
+  return <Comp className={cn("hover:text-foreground transition-colors", className)} {...props} />;
+};
 
-const BreadcrumbPage = React.forwardRef<HTMLSpanElement, React.ComponentPropsWithoutRef<"span">>(
-  ({ className, ...props }, ref) => (
-    <span
-      ref={ref}
-      role="link"
-      aria-disabled="true"
-      aria-current="page"
-      className={cn("text-foreground font-normal", className)}
-      {...props}
-    />
-  )
+export const BreadcrumbPage = ({ className, ...props }: React.ComponentProps<"span">) => (
+  <span
+    role="link"
+    aria-disabled="true"
+    aria-current="page"
+    className={cn("text-foreground font-normal", className)}
+    {...props}
+  />
 );
-BreadcrumbPage.displayName = "BreadcrumbPage";
 
-const BreadcrumbSeparator = ({ children, className, ...props }: React.ComponentProps<"li">) => (
+export const BreadcrumbSeparator = ({ children, className, ...props }: React.ComponentProps<"li">) => (
   <li role="presentation" aria-hidden="true" className={cn("[&>svg]:h-3.5 [&>svg]:w-3.5", className)} {...props}>
     {children ?? <ChevronRight />}
   </li>
 );
-BreadcrumbSeparator.displayName = "BreadcrumbSeparator";
 
-const BreadcrumbEllipsis = ({ className, ...props }: React.ComponentProps<"span">) => (
+export const BreadcrumbEllipsis = ({ className, ...props }: React.ComponentProps<"span">) => (
   <span
     role="presentation"
     aria-hidden="true"
@@ -76,14 +62,3 @@ const BreadcrumbEllipsis = ({ className, ...props }: React.ComponentProps<"span"
     <span className="sr-only">More</span>
   </span>
 );
-BreadcrumbEllipsis.displayName = "BreadcrumbElipssis";
-
-export {
-  Breadcrumb,
-  BreadcrumbList,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-  BreadcrumbEllipsis
-};
